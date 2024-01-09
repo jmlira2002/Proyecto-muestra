16dab75c72d33caffa1c75770b21dc22c079f35e
jmlira2002 <145620199+jmlira2002@users.noreply.github.com>
2024-01-08 23:35:56 -0600

    Update integradora.py

--- integradora.py
+++ integradora.py
@@ -142,7 +142,7 @@ grouped_data = subset_data4.groupby(['Incident Category', 'Incident Subcategory'
 grouped_data['Percentage'] = grouped_data['Total Crimes'] / grouped_data['Total Crimes'].sum() * 100
 
 
-figcrimes = px2.sunburst(grouped_data, path=['Incident Category', 'Incident Subcategory'], values='Total Crimes', color='Percentage', color_continuous_scale='ice',title='Crimes by Category and Subcategory')
+figcrimes = px2.sunburst(grouped_data, path=['Incident Category', 'Incident Subcategory'], values='Total Crimes', color='Percentage', title='Crimes by Category and Subcategory')
 #figcrimes.update_traces(hovertemplate='<b>%{label}</b><br>Total Crimes: %{value}<br>Percentage: %{color:.2f}%')
 #figcrimes.update_layout(width = 400)
 
